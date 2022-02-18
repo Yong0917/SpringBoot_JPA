5450a4a4e4dcf6af01f712246d487461d945b257
yong0917 <ssyong917@naver.com>
2022-02-18 18:22:00 +0900

    초기 세팅 0218

---
+++ src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.Entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //test 코드 단축키 커맨드 쉬프트 t
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
